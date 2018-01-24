99e6e76204d34d7ef98eb0ec677a48f0eeaa9f9a
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-01-23 21:28:39 -0600

    flexed hero image

--- js/yougotthis.js
+++ js/yougotthis.js
@@ -1,11 +1,11 @@
-var heroImage0 = document.getElementById('heroImage0'); 
+var heroImage = document.getElementById('heroImage'); 
 // var heroImage1 = document.getElementById('heroImage1');
 // var heroImage2 = document.getElementById('heroImage2'); 
 
 var heroes = [ 
-    `<img src="../images/Band.jpg">`,
-    `<img src="../images/hero_stem.jpg">`,
-    `<img src="../images/Archery.jpg">`
+    `<img src="../images/Band.jpg" style="display: flex; margin: auto; max-width: 100%;">`,
+    `<img src="../images/hero_stem.jpg" style="display: flex; margin: auto; max-width: 100%;">`,
+    `<img src="../images/Archery.jpg" style="display: flex; margin: auto; max-width: 100%;">`
     ]
 
 var random = Math.floor(Math.random() * Math.floor(heroes.length));
@@ -14,13 +14,13 @@ console.log(random);
 
 switch(random){
     case 0:
-        heroImage0.innerHTML = heroes[0];
+        heroImage.innerHTML = heroes[0];
         break;
     case 1:
-        heroImage0.innerHTML = heroes[1];
+        heroImage.innerHTML = heroes[1];
         break;
     case 2:
-        heroImage0.innerHTML = heroes[2];
+        heroImage.innerHTML = heroes[2];
         break;
 }
 
