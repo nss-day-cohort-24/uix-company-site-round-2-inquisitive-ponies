4eb91b6afd9dca9d076d291f04a3fe97180ef735
Taylor Bailey <taylor.williams.bailey@gmail.com>
2018-01-24 09:51:34 -0600

    used javascript on contact page

---
+++ js/contact.js
@@ -0,0 +1,5 @@
+document.getElementById("callToAction").innerHTML = "Get in touch! We look forward to finding the best program for your child!";
+document.getElementById("formName").innerHTML = "Name: (required)";
+document.getElementById("formTele").innerHTML = "Telephone: (required)";
+document.getElementById("formEmail").innerHTML = "Email: (required)";
+document.getElementById("formMessage").innerHTML = "Message: ";
